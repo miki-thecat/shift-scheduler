8d9ed8971e889dafb9264bcd75195c8097e24fb2
miki-thecat <Kanato.erika773@gmail.com>
2025-08-10 19:01:40 +0900

    app.pyを書き始めた

--- shift-api/app.py
+++ shift-api/app.py
@@ -2,4 +2,18 @@ import os
 from datetime import datetime
 from flask import Flask, request
 from flask_cors import CORS
+from MySQLdb
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+DB = MySQLdb.connect(
+    host=os.getenv("DB_HOST", "db"),
+    user=os.getenv("DB_USER", "user"),
+    password=os.getenv("DB_PASSWORD", "pass"),
+    db=os.getenv("DB_NAME", "app"),
+    charset="utf8mb4",
+    autocommit=True
+)
+
 
